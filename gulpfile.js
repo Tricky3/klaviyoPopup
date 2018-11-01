bc07ab0b69853a4e2153e6b300514e5c0b062c92
jayvin <jayvin86@gmail.com>
2018-11-01 15:58:57 +0400

    Updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,36 +6,37 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var del = require('del');
 
+// identify shopify assets
+const ASSETS = './assets/';
+const JS_SCRIPTS = [`${ASSETS}*.js`, `!${ASSETS}*.min.js`];
+
 // gulp plumber error handler
-var onError = function(err){
+const onError = (err) => {
   console.log(err);
 }
 
-// identify Shopify assets
-var assets = './assets/'
-var jsScripts = [assets + '*.js', '!' + assets + '*.min.js'];
+const scripts = () => {
+  return gulp.src(JS_SCRIPTS)
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe(rename(function(script){
+      script.extname = '.min.js';
+      console.log(script);
+      return script;
+    }))
+    .pipe(stripDebug())
+    .pipe(uglify())
+    .pipe(gulp.dest(ASSETS))
+    .pipe(concat('tricky3.klaviyo.pack.min.js'))
+    .pipe(gulp.dest(ASSETS));
+}
 
-// uglify custom scripts
-gulp.task('scripts', function(){
-  return gulp.src(jsScripts)
-  .pipe(plumber({
-    errorHandler:onError
-  }))
-  .pipe(rename(function(script) {
-    script.extname = '.min.js';
-    return script;
-  }))
-  .pipe(stripDebug())
-  .pipe(uglify())
-  .pipe(gulp.dest(assets))
-  .pipe(concat('tricky3.klaviyo.pack.min.js'))
-  .pipe(gulp.dest(assets));
-});
+const watcher = () => {
+  const tasks = ['scripts'];
+  gulp.watch([ASSETS, JS_SCRIPTS], gulp.parallel(tasks));
+}
 
-// Watch
-gulp.task('watch', function(){
-	var tasks = ['scripts'];
-	gulp.watch([assets,jsScripts], tasks);
-});
+exports.scripts = scripts;
+exports.default = gulp.parallel(watcher);
 
-gulp.task('default', ['watch']);
